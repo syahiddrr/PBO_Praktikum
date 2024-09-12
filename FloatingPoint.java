1923f97cb6d034e1da85cd8739dc7536dec67f61
Muammar Syahid Rahmallah <143985336+syahiddrr@users.noreply.github.com>
2024-09-12 14:48:28 +0700

    Delete FloatingPoint.java

--- FloatingPoint.java
+++
@@ -1,7 +0,0 @@
-public class FloatingPoint {
-    public static void main(String[] args) {
-        double x = 92.98;
-        int nx = (int) Math.round(x);
-        System.out.print("Hasil dari pembulatan : " + nx);
-    }
-}
